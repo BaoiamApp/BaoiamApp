7a6a10e0d2a8725cb2f5184885065f034a0fb9c0
peerzadaburhan <peerzadaburhan7@gmail.com>
2024-03-15 16:47:49 +0530

    Adds onboarding screen one

--- app/src/main/java/com/baoiaminnovations/baoiamapp/MainActivity.kt
+++ app/src/main/java/com/baoiaminnovations/baoiamapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.baoiaminnovations.baoiamapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.baoiaminnovations.baoiamapp.common.presentation.NavHost
 import com.baoiaminnovations.baoiamapp.ui.theme.BaoiamAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             BaoiamAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column {
+                    NavHost(navHostController = rememberNavController())
                 }
             }
         }
--- app/src/main/java/com/baoiaminnovations/baoiamapp/onboardingfeature/screens/OnBoardingScreen1.kt
+++ app/src/main/java/com/baoiaminnovations/baoiamapp/onboardingfeature/screens/OnBoardingScreen1.kt
@@ -1,8 +1,110 @@
 package com.baoiaminnovations.baoiamapp.onboardingfeature.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import com.baoiaminnovations.baoiamapp.R
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
-fun OnBoardingScreen1(){
+fun OnBoardingScreen1() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.onPrimary)
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcomeText),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 20.dp, start = 10.dp),
+            style = TextStyle(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFFFF7B00),
+                        Color(0xFFFF0400)
+                    )
+                )
+            )
+        )
+        Box {
+            Text(
+                text = stringResource(id = R.string.welcomeText2),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 5.dp, start = 10.dp).zIndex(10f),
+                color = Color.Gray
+            )
+            Image(
+                painter = painterResource(id = R.drawable.onboarding1),
+                contentDescription = "Onboarding",
+                modifier = Modifier
+                    .width(400.dp)
+                    .height(550.dp)
+                    .padding(10.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .width(350.dp)
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 50.dp),
+            shape = MaterialTheme.shapes.medium.copy(all = CornerSize(10.dp)),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            ),
+            contentPadding = PaddingValues()
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            listOf(
+                                Color(0xFFFF7B00),
+                                Color(0xFFFF0400)
+                            )
+                        )
+                    )
+                    .width(350.dp)
+                    .height(45.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = stringResource(id = R.string.next))
+            }
 
+        }
+    }
 }
