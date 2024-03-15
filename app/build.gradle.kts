7d6b69c35d3e7777b9930b2f2253c8d40c3d1a47
deepe <deepeshrajpoot001@gmail.com>
2024-03-15 07:11:15 +0530

    README.md add and update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.baoiaminnovations.baoiamapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.baoiaminnovations.baoiamapp"
