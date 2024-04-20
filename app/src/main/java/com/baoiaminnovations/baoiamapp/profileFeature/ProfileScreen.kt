149402d1e141002af7e8d352be801842eb63ce2a
jayesh chopada <cjayesh1995@gmail.com>
2024-04-20 18:31:26 +0530

    Added Alert Dialog box

--- app/src/main/java/com/baoiaminnovations/baoiamapp/profileFeature/ProfileScreen.kt
+++ app/src/main/java/com/baoiaminnovations/baoiamapp/profileFeature/ProfileScreen.kt
@@ -1,43 +1,30 @@
 package com.baoiaminnovations.baoiamapp.profileFeature
 
-import android.provider.CalendarContract.Colors
+
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-
 import androidx.compose.foundation.layout.Column
-
-
-
 import androidx.compose.foundation.layout.Row
-
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bookmark
-import androidx.compose.material.icons.filled.CheckBox
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.AddAPhoto
-import androidx.compose.material.icons.outlined.ArrowOutward
-import androidx.compose.material.icons.outlined.Bookmark
-import androidx.compose.material.icons.outlined.Logout
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -46,22 +33,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -75,6 +57,7 @@ import com.baoiaminnovations.baoiamapp.ui.theme.LinearGradient2
 @Composable
 fun ProfileScreen(navHostController: NavHostController) {
 
+    var isdialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -89,7 +72,7 @@ fun ProfileScreen(navHostController: NavHostController) {
         ) {
             Row(
                 modifier = Modifier.align(Alignment.CenterStart),
-                verticalAlignment = Alignment.CenterVertically,
+                verticalAlignment = CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ) {
                 Image(painter = painterResource(id = R.drawable.profile),
@@ -279,7 +262,7 @@ fun ProfileScreen(navHostController: NavHostController) {
 
         TextButton(
             onClick = { 
-
+                isdialog = true
             }, colors = ButtonDefaults.textButtonColors(
             ), modifier = Modifier.align(CenterHorizontally)
         ) {
@@ -292,6 +275,119 @@ fun ProfileScreen(navHostController: NavHostController) {
                 modifier = Modifier.padding(start = 10.dp)
             )
         }
+        if (isdialog){
+            BasicAlertDialog(
+                onDismissRequest = { isdialog = false },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(15.dp))
+            ) {
+
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = "See you soon!", modifier = Modifier
+                            .align(CenterHorizontally)
+                            .padding(top = 30.dp),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold)
+
+                    Text(
+                        text = stringResource(id = R.string.logout_text),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .align(CenterHorizontally)
+                            .padding(top = 20.dp),
+                        fontSize = 16.sp)
+
+
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = CenterVertically
+                        ,modifier = Modifier
+                            .fillMaxWidth()
+                            .height(46.dp)
+
+                    ) {
+
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+
+                        ) {
+                            OutlinedButton(onClick = { isdialog = false },
+                                modifier = Modifier.height(75.dp).width(130.dp),
+                                shape = RoundedCornerShape(10.dp),
+                                border = BorderStroke(width = 2.dp,
+                                    brush = Brush.horizontalGradient(colors = listOf(
+                                        LinearGradient, LinearGradient2
+                                    )))
+
+
+                            ) {
+                                Text(
+                                    text = "Cancel",
+                                    color = Color.Black,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 15.sp
+                                )
+                            }
+                        }
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .width(130.dp)
+                        ) {
+                            Button(onClick = {}, modifier = Modifier
+                                .height(60.dp)
+                                .background(
+                                    brush = Brush.horizontalGradient(
+                                        colors = listOf(
+                                            LinearGradient,
+                                            LinearGradient2
+                                        )
+                                    ), shape = RoundedCornerShape(10.dp)
+                                ),
+
+                                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+
+
+                            ) {
+                                Text(text = "Yes, Logout", fontSize = 15.sp, color = Color.White)
+                            }
+                        }
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(75.dp)
+                            .padding(top = 20.dp)
+                    ) {
+
+                        Image(
+                            painter = painterResource(id = R.drawable.check_box),
+                            contentDescription ="" ,
+                            modifier = Modifier.clickable {  }
+                        )
+
+                        Text(
+                            text = "Remember log in details",
+                            modifier = Modifier
+                                .padding(start = 20.dp)
+                        )
+                    }
+
+                }
+
+            }
+        }
+
 
     }
 
