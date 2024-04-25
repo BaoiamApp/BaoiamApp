e8f91c93b9382866998983505a60ad516da2072b
Peerzada Burhan Ahmad <peerzadaburhan7@gmail.com>
2024-04-25 12:12:54 +0530

    Adds firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -83,4 +84,6 @@ dependencies {
     //COIL
     implementation("io.coil-kt:coil-compose:2.6.0")
 
+    //FIREBASE
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
 }
